e1a1a59b50e6345b2ac0e028276554cfc147e211
morizkay <morizkay@live.com>
2022-10-09 03:31:16 +0800

    reactor-i18n: footer page

--- core/components/Footer.tsx
+++ core/components/Footer.tsx
@@ -1,8 +1,35 @@
 import Link from 'next/link';
 import { whatsappLink, whatsappNo } from '../lib/url';
 import styles from './Footer.module.css';
+import {useTranslations} from "next-intl";
 
 export default function Footer () {
+  const t = useTranslations("footer")
+
+  const socials: Array<{
+    name: string,
+    link: string
+  }>=[
+    {
+      name:"Discord",
+      link: "https://discord.com/invite/M9mNK6MBbu"
+    },
+    {
+      name:"Instagram",
+      link: "https://www.instagram.com/hmrcode/"
+    },
+    {
+      name:"Facebook",
+      link: "https://www.facebook.com/groups/695578487303771/"
+    },
+    {
+      name:"Github",
+      link: "https://github.com/hammer-code"
+    }
+    ]
+
+  const address = "Jl. Lagarutu No. 28, Tanamodindi, Palu"
+
   const year = new Date().getFullYear();
   return (
     <footer>
@@ -11,23 +38,27 @@ export default function Footer () {
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div></div>
         <div className="px-4">
-          <h2 className="text-lg font-bold mb-4">Resources</h2>
+          <h2 className="text-lg font-bold mb-4">{t('resources')}</h2>
           <ul>
-            <li className="mb-2"><Link className="link" href="/">Home</Link></li>
-            <li className="mb-2"><Link className="link" href="/about">Tentang Kami</Link></li>
+            <li className="mb-2"><Link className="link" href="/">{t('home')}</Link></li>
+            <li className="mb-2"><Link className="link" href="/about">{t('about_us')}</Link></li>
           </ul>
         </div>
         <div className="px-4">
-          <h2 className="text-lg font-bold mb-4">Ayo Gabung</h2>
+          <h2 className="text-lg font-bold mb-4">{t('lets_join')}</h2>
           <ul>
-            <li className="mb-2"><a className="link" target="_blank" href="https://discord.com/invite/M9mNK6MBbu">Discord</a></li>
-            <li className="mb-2"><a className="link" target="_blank" href="https://www.instagram.com/hmrcode/">Instagram</a></li>
-            <li className="mb-2"><a className="link" target="_blank" href="https://www.facebook.com/groups/695578487303771/">Facebook</a></li>
-            <li className="mb-2"><a className="link" target="_blank" href="https://github.com/hammer-code">Github</a></li>
+            {
+              socials.map((social)=> {
+                return (
+
+                  <li className="mb-2"><a className="link" target="_blank" href={social.link}>{social.name}</a></li>
+                )
+              })
+            }
           </ul>
         </div>
         <div className="px-4">
-        <h2 className="text-lg font-bold mb-4">Kontak</h2>
+        <h2 className="text-lg font-bold mb-4">{t('contacts' )}</h2>
           <ul>
             <li>
               <a className="text-secondary underline" href={whatsappLink} target="_blank">Whatsapp {whatsappNo}</a>
@@ -36,8 +67,7 @@ export default function Footer () {
               <a className="text-secondary underline" href="mailto:hammercode28@gmail.com">hammercode28@gmail.com</a>
             </li>
           </ul>
-
-          <p><a href="https://goo.gl/maps/6BS2dTYcE7wws94w6" target="_blank">Jl. Lagarutu No. 28, Tanamodindi, Palu</a></p>
+          <p><a href="https://goo.gl/maps/6BS2dTYcE7wws94w6" target="_blank">{address}</a></p>
         </div>
       </div>
       </div>
