3fa1aebb9f5d2731b2568404a99158e7a30932f7
morizkay <morizkay@live.com>
2022-10-14 21:40:44 +0800

    refactor-i18n: create i18n source for home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
 import {NextIntlProvider} from 'next-intl';
 import Head from 'next/head';
 import '../styles/main.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
+
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+        <title>{"Hammercode"}</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"/>
       </Head>
       <Component {...pageProps} />
     </NextIntlProvider>
