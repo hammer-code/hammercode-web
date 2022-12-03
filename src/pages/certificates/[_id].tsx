6188942d94947a05142d5dcaf10075386c813bff
morizkay <morizkay@live.com>
2022-12-03 21:51:50 +0800

    feat: add meta tags

--- src/pages/certificates/[_id].tsx
+++ src/pages/certificates/[_id].tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import Image from 'next/image';
+import Head from 'next/head';
 
 const Certificate: NextPage<{
   certificate: {
@@ -11,70 +12,84 @@ const Certificate: NextPage<{
   };
 }> = ({ certificate: { _id, name, imageLink, shareLink } }): JSX.Element => {
   return (
-    <div className="w-full">
-      <div className="p-4 my-12">
-        <p className="text-2xl font-bold text-center">
-          Terima Kasih kepada {name}!
-        </p>
-        <p className="text-center text-gray-500">
-          Kamu telah berpartisipasi dalam acara ini.
-        </p>
-        <div className="w-full h-full relative">
-          <div className="flex justify-center">
-            <Image
-              src={imageLink}
-              className="my-4 rounded-lg shadow-md object-cover object-center"
-              width="640"
-              height="480"
-            />
-          </div>
+    <>
+      <Head>
+        <title>Palu Dev Day 2022</title>
+        <meta property="og:title" content={`PDD 2022 - ${name}`} key="title" />
+        <meta
+          property="og:description"
+          content="Terima Kasih Kamu Sudah Berpartisipasi di Palu Developer Day 2022"
+          key="description"
+        />
+        <meta property="og:image" content={imageLink} key="image" />
+      </Head>
+      <div className="w-full">
+        <div className="p-4 my-12">
+          <p className="text-2xl font-bold text-center">
+            Terima Kasih kepada {name}!
+          </p>
+          <p className="text-center text-gray-500">
+            Kamu telah berpartisipasi dalam acara ini.
+          </p>
+          <div className="w-full h-full relative">
+            <div className="flex justify-center">
+              <Image
+                src={imageLink}
+                className="my-4 rounded-lg shadow-md object-cover object-center"
+                width="640"
+                height="480"
+              />
+            </div>
+            <p className="text-center text-gray-300 mb-8">{_id}</p>
 
-          <div className="flex justify-center">
-            <a
-              href={shareLink}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              target={'_blank'}
-            >
-              UNDUH SERTIFIKAT
-            </a>
-          </div>
-          <div>
-            <p className="text-center text-gray-500 my-4">
-              <strong>Pamerkan</strong> sertifikat ini ke teman-temanmu!
-            </p>
-
-            <div className="flex justify-center space-x-6">
+            <div className="flex justify-center">
               <a
-                href={`https://www.facebook.com/sharer/sharer.php?u=${shareLink}`}
-              >
-                <FaFacebook
-                  className="w-full text-3xl text-blue-500 hover:text-blue-700"
-                  size={32}
-                />
-              </a>
-              <a
-                href={`https://twitter.com/intent/tweet?text=Saya telah mengikuti acara Palu Developer Day yang diadakan oleh Hammercode.org&url=${shareLink}`}
+                href={shareLink}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
                 target={'_blank'}
               >
-                <FaTwitter
-                  className="w-full text-3xl text-blue-500 hover:text-blue-700"
-                  size={32}
-                />
+                UNDUH SERTIFIKAT
               </a>
-              <a
-                href={`https://www.linkedin.com/shareArticle?mini=true&url=${shareLink}`}
-                target={'_blank'}
-              >
-                <FaLinkedin
-                  className="text-3xl text-blue-500 hover:text-blue-700"
-                  size={32}
-                />
-              </a>
+            </div>
+            <div>
+              <p className="text-center text-gray-500 my-4">
+                <strong className="text-lg text-black">Pamerkan</strong>{' '}
+                sertifikat ini ke teman-temanmu!
+              </p>
+
+              <div className="flex justify-center space-x-6">
+                <a
+                  href={`https://www.facebook.com/sharer/sharer.php?u=${shareLink}`}
+                >
+                  <FaFacebook
+                    className="w-full text-3xl text-blue-500 hover:text-blue-700"
+                    size={32}
+                  />
+                </a>
+                <a
+                  href={`https://twitter.com/intent/tweet?text=Saya telah mengikuti acara Palu Developer Day yang diadakan oleh Hammercode.org&url=${shareLink}`}
+                  target={'_blank'}
+                >
+                  <FaTwitter
+                    className="w-full text-3xl text-blue-500 hover:text-blue-700"
+                    size={32}
+                  />
+                </a>
+                <a
+                  href={`https://www.linkedin.com/shareArticle?mini=true&url=${shareLink}`}
+                  target={'_blank'}
+                >
+                  <FaLinkedin
+                    className="text-3xl text-blue-500 hover:text-blue-700"
+                    size={32}
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
