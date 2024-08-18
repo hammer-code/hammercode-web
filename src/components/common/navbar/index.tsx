2c1cc2cb4a3acd7909e30078e78ab0aeb9605456
mpsalunggg <putrasatria893@gmail.com>
2024-08-19 00:34:26 +0700

    chore: icon image logo

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,24 +1,19 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 
 import NavbarList from "./NavbarList";
 import { navbarLists } from "./constants";
 import { ModeToggle } from "../modetoggle";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 
 const Navbar = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
   return (
     <header className="border-b">
       <div className="max-w-7xl mx-auto p-5">
         <div className="flex items-center justify-between gap-4">
           <Link href="/">
-            <Image
-              src={`/assets/icons/${theme === "dark" ? "ic_hmc-dark.svg" : "ic_hmc-light.svg"}`}
-              alt="hammercode logo"
-              width={40}
-              height={36}
-            />
+            <div className="w-10 h-8 bg-[url('/assets/icons/ic_hmc-light.svg')] dark:bg-[url('/assets/icons/ic_hmc-dark.svg')] bg-cover bg-center"></div>
           </Link>
 
           <nav className="flex items-center gap-4">
