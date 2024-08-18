c5c8036fb82c8b2f9ea70885358f6f031d10a525
vickyadri29 <vickyadri290501@gmail.com>
2024-08-18 23:37:30 +0700

    feat: create navbar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Image from "next/image";
+
 import NavbarList from "./NavbarList";
 import { navbarLists } from "./constants";
 
@@ -7,7 +9,9 @@ const Navbar = () => {
     <header className="bg-white border-b">
       <div className="max-w-7xl mx-auto p-5">
         <div className="flex items-center justify-between gap-4">
-          <Image src="/assets/icons/ic_hmc-light.svg" alt="hammercode logo" width={40} height={36} />
+          <Link href="/">
+            <Image src="/assets/icons/ic_hmc-light.svg" alt="hammercode logo" width={40} height={36} />
+          </Link>
 
           <nav className="flex items-center gap-4">
             {navbarLists.map(({ link, title }) => (
