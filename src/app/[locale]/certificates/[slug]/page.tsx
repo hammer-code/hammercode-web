7955bac768f92a6fca54f2b97707d4ae17103ba1
vickyadri29 <vickyadri290501@gmail.com>
2024-09-16 15:45:37 +0700

    fix: error build when fetching the certificate

--- src/app/[locale]/certificates/[slug]/page.tsx
+++ src/app/[locale]/certificates/[slug]/page.tsx
@@ -3,18 +3,6 @@ import { notFound } from "next/navigation";
 import Certificate from "@/features/certificate/Certificate";
 import { CertificateType } from "@/features/certificate/types";
 
-export async function getCertificate(slug: string) {
-  try {
-    const response = await fetch(`https://moonlight.hammercode.org/v1/certificates/${slug}`);
-    if (!response.ok) return null;
-
-    return response.json();
-  } catch (error) {
-    console.error("Error fetching certificate:", error);
-    return null;
-  }
-}
-
 type Props = {
   params: {
     slug: string;
@@ -22,12 +10,14 @@ type Props = {
 };
 
 const CertificateDetail = async ({ params }: Props) => {
-  const certificate: CertificateType = await getCertificate(params.slug);
-  if (!certificate.name) {
+  const data = await fetch(`https://moonlight.hammercode.org/v1/certificates/${params.slug}`);
+  const certificateDetail: CertificateType = await data.json();
+
+  if (!certificateDetail.name) {
     return notFound();
   }
 
-  return <Certificate certificate={certificate} />;
+  return <Certificate certificate={certificateDetail} />;
 };
 
 export default CertificateDetail;
